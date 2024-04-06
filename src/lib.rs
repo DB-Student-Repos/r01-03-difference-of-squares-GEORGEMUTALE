75e3a5c197ecc675a43d6ed4c5354a15b05d0d7a
MUTALE GEORGE <148863672+GEORGEMUTALE@users.noreply.github.com>
2024-04-06 12:11:36 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,26 @@
-pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+use std::io;
+
+fn main(){
+    println!("Enter N which represents first natural numbers");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let n: u32 = input.trim().parse().expect("Please enter a valid Number");
+
+    let difference = difference(n);
+
+    println!("{}", difference)
 }
 
+pub fn difference(n:u32) ->u32{
+    let squares  = sum_of_squares(n);
+    let square_of_sum = square_of_sum(n);
+    square_of_sum - squares
+}
+
+pub fn square_of_sum(n:u32) -> u32{
+    let sum = (1..=n).sum::<u32>();
+    sum * sum
+}
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
-}
-
-pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    (1..=n).map(|x| x * x).sum::<u32>()
 }
